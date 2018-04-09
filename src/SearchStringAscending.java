7fa0cf941661fb55b7c01e42ee37fee12163316e
khoand10@gmail.com <khoand10@gmail.com>
2018-04-09 20:52:12 +0700

    commit [Bài tập] Tìm chuỗi tăng dần có độ dài lớn nhất

---
+++ src/SearchStringAscending.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class SearchStringAscending {
+    static LinkedList<Character> searchStringAscending(String string) {
+        LinkedList<Character> max = new LinkedList<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            LinkedList<Character> list = new LinkedList<>();
+            list.add(string.charAt(i));
+            for (int j = i + 1; j < string.length(); j++) {
+                if (string.charAt(j) > list.getLast()) {
+                    list.add(string.charAt(j));
+                }
+            }
+
+            if (list.size() > max.size()) {
+                max.clear();
+                max.addAll(list);
+            }
+            list.clear();
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String string = sc.nextLine();
+        for (char c : searchStringAscending(string)) {
+            System.out.print(c + "\t");
+        }
+    }
+}
